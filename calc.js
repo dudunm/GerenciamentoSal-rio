35ea24afcfb8832edd16f897cbe990b6aa789be3
Eduardo Nienkotter Marcelino <110943998+dudunm@users.noreply.github.com>
2025-04-30 00:19:28 -0300

    Add files via upload

--- calc.js
+++ calc.js
@@ -5,6 +5,9 @@ let trinta = document.getElementById("trinta")
 let vinte = document.getElementById("vinte")
 let erro = document.getElementById("erro")
 let dica = document.getElementById("dica")
+let txtResposta = document.getElementById("txtResposta")
+
+dica.setAttribute("hidden", "hidden")
 
 calcular.addEventListener("click", (e) => {
 
@@ -17,25 +20,29 @@ calcular.addEventListener("click", (e) => {
         cinquenta.hidden = false
         trinta.hidden = false
         vinte.hidden = false
-        dica.hidden = false
+        txtResposta.hidden = false
+
+        dica.removeAttribute("hidden")
 
         let salario50 = salarioo*0.5
         let salario30 = salarioo*0.3
         let salario20 = salarioo*0.2
 
-        dica.innerHTML = "Separe seu salário da seguinte forma:"
+        txtResposta.innerHTML = "Separe seu salário da seguinte forma:"
 
         cinquenta.innerHTML = `50% do salário: R$${salario50.toFixed(2)}`
         trinta.innerHTML = `30% do salário: R$${salario30.toFixed(2)}`
         vinte.innerHTML = `20% do salário: R$${salario20.toFixed(2)}`
     }else{
 
+        erro.hidden = false
+
         cinquenta.hidden = true
         trinta.hidden = true
         vinte.hidden = true
-        dica.hidden = true
+        txtResposta.hidden = true
 
-        erro.hidden = false
+        dica.setAttribute("hidden", "hidden")
 
         erro.innerHTML = "O valor deve ser maior que 0"
     }
